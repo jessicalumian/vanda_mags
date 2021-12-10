67c989cff8004877b92f00da90c653f5c804679c
jessicalumian <jessica.mizzi@gmail.com>
2021-12-10 12:14:09 -0800

    Update annual_irradiance.R

--- annual_irradiance.R
+++ annual_irradiance.R
@@ -60,7 +60,7 @@ myPlot <- ggplot(PARMinMax2012long, aes(DATE_TIME, value, colour = variable)) + 
   scale_x_date(limit=c(as.Date("2011-07-01"),as.Date("2012-07-01")), date_labels = "%B %Y",date_breaks ="1 month") +
   scale_y_continuous(expand = expansion(mult = c(0, 0.01))) +
   theme(axis.text.x=element_text(angle=70, hjust=1), panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  labs(y="Photosynthetically Active Radiation (PAR)", x = "Month") +
+  labs(y=bquote("Photosynthetically Active Radiation in photons " ~m^-2 ~s^-1), x = "Month") +
   scale_colour_discrete(name="PAR Measurement", labels=c("Average PAR", "Minimum PAR", "Maximum PAR")) +
   ggtitle("Irradiance in Lake Vanda July 2011 - July 2012") +
   # add date annotations, the entries in the season column are used as reference but not called directly
@@ -72,10 +72,8 @@ myPlot <- ggplot(PARMinMax2012long, aes(DATE_TIME, value, colour = variable)) + 
   annotate(geom="text", x=(as.Date("2011-07-22")), y=1700, label="Winter", color="black") + 
   annotate(geom="text", x=(as.Date("2011-09-10")), y=1700, label="Spring", color="blue") + 
   annotate(geom="text", x=(as.Date("2011-11-22")), y=1700, label="Summer", color="green") + 
-  annotate(geom="text", x=(as.Date("2012-03-13")), y=1700, label="Spring", color="blue") + 
+  annotate(geom="text", x=(as.Date("2012-03-13")), y=1700, label="Fall", color="blue") + 
   annotate(geom="text", x=(as.Date("2012-05-22")), y=1700, label="Winter", color="black")
   
 
 myPlot
-
-
